06b6fb00205954c10b7a810af3e04121001e785c
sahhan99 <binmusfer3@gmail.com>
2026-01-18 00:30:16 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // هام جداً:
-  // إذا كان اسم المستودع sahhan99 اتركها كما هي
-  // إذا كان المستودع هو sahhan99.github.io مباشرة، اجعلها '/'
-  base: '/sahhan99/', 
+  // ⚠️ هام: هذا المسار يجب أن يطابق اسم المستودع (Repository)
+  base: '/sahhan-x99/', 
 })
