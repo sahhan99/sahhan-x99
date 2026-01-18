9cdc75b64372c67ac16f413934560979da108c39
sahhan99 <binmusfer3@gmail.com>
2026-01-18 06:35:54 +0300

    Update api.ts

--- src/core/api.ts
+++ src/core/api.ts
@@ -1,5 +1,5 @@
 export async function sendToCore(command: string) {
-  const res = await fetch("http://127.0.0.1:8080/api/command", {
+  const res = await fetch("http://192.168.8.16:8080/api/command", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ command })
