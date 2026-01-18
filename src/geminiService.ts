b372f022e03b8540a287aebe3e94531f60c8641c
sahhan99 <binmusfer3@gmail.com>
2026-01-18 03:04:21 +0300

    Update geminiService.ts

--- src/geminiService.ts
+++ src/geminiService.ts
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 // تم حقن مفتاح السيادة الخاص بك هنا
-const API_KEY = "AIzaSyA-nqDiMGYSvoYmoxrNOPmjRDf4SVZlP2M"; 
+const API_KEY = "AIzaSyBXJvl8iP6-eYtk6WEZ5-9A-7bRSqO7RJw"; 
 
 const genAI = new GoogleGenerativeAI(API_KEY);
 
